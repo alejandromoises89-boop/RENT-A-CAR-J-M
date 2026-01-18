313be482b76839e4c106a85079614fe520da8636
alejandromoises89-boop <alejandromoises89@gmail.com>
2026-01-18 18:27:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from datetime import datetime, date, timedelta, time
 import urllib.parse
 import calendar
 import styles
-import stramlit as st
 from stramlit_gsheets import GSheetsConnection
 
 # --- CONFIGURACIÓN DE PÁGINA ---
